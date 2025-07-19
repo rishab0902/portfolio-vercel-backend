1f949708437967dd75a78f20036c9fe3617b583a
Kumar Rishab <rishab.davksd@gmail.com>
2025-07-19 20:35:33 +0530

    new changes for deployment

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Enable CORS
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3001',
+  credentials: true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
